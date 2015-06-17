673daea3c433e994e6499aed14caaebf10799343
Maxim Golubew <Maxim.Golubew@me.com>
2015-06-17 15:37:32 +0200

    Version 1.0 des HTML-Dummy

--- web/main.dart
+++ web/main.dart
@@ -1,8 +1,7 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// Copyright (c) 2015, <Max Golubew, Antonia Berg & Berengar Doege>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:html';
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
 }
